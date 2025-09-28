f462398ec77623e1bc35b64c519d3e8a93ca2a6c
madina875 <evaevasmq@gmail.com>
2025-09-28 10:53:10 +0500

    hmw

--- src/features/movie-date/ui/MovieDate.tsx
+++ src/features/movie-date/ui/MovieDate.tsx
@@ -1,20 +1,22 @@
 import { DatePicker, Space, type DatePickerProps } from "antd";
 import { memo, useState } from "react";
 // import { useSearchParams } from "react-router-dom";
-import { useMovie } from "../../../entities/movie";
+// import { useMovie } from "../../../entities/movie";
 
 export const MovieDate = memo(() => {
   const [startDate, setStartDate] = useState<any>("");
   const [endtDate, setEndDate] = useState<any>("");
-  const { getMovieGenreList } = useMovie();
-  const { data } = getMovieGenreList();
+  //   const { getMovieGenreList } = useMovie();
+  //   const { data } = getMovieGenreList();
 
   const onChangeStart: DatePickerProps["onChange"] = (date, dateString) => {
     setStartDate(dateString);
     console.log(startDate);
+    console.log(date);
   };
   const onChangeEnd: DatePickerProps["onChange"] = (date, dateString) => {
     setEndDate(dateString);
+    console.log(date);
     console.log(endtDate);
   };
 
