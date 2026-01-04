efb45e55227c17a1bfa76bbff4da9991c7931124
madina875 <evaevasmq@gmail.com>
2026-01-04 19:06:34 +0500

    asd

--- src/pages/tickets/ui/Tickets.tsx
+++ src/pages/tickets/ui/Tickets.tsx
@@ -3,21 +3,22 @@ import { useSelector } from "react-redux";
 import type { RootState } from "../../../app/store";
 import { MovieCard, type IMovie } from "../../../entities/movie";
 import { Empty } from "antd";
-
 export const Tickets = memo(() => {
   const carts = useSelector((state: RootState) => state.cart.value);
 
+  if (!carts || carts.length === 0) {
+    return (
+      <div className="container flex items-center justify-center min-h-[60vh]">
+        <Empty imageStyle={{ height: 120 }} />
+      </div>
+    );
+  }
+
   return (
-    <div className="container grid grid-cols-5 gap-5 my-10">
-      {carts && carts.length > 0 ? (
-        carts?.map((item: IMovie) => <MovieCard key={item.id} movie={item} />)
-      ) : (
-        <div className="flex pl-180 items-center justify-center">
-          <div className="mt-20">
-            <Empty className="size-100" />
-          </div>
-        </div>
-      )}
+    <div className="container grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-5 my-10">
+      {carts.map((item: IMovie) => (
+        <MovieCard key={item.id} movie={item} />
+      ))}
     </div>
   );
 });
